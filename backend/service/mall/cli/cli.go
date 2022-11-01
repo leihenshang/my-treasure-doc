5ec987860d1f815a483fd22aa6aa1e987c8399e0
tangzq <t1339804847@163.com>
2022-11-01 17:31:34 +0800

    feat:添加代码生成

--- backend/service/mall/cli/cli.go
+++ backend/service/mall/cli/cli.go
@@ -1,6 +1,10 @@
 package main
 
-import "gorm.io/gen"
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+)
 
 // Dynamic SQL
 type Querier interface {
@@ -10,18 +14,15 @@ type Querier interface {
 
 func main() {
 	g := gen.NewGenerator(gen.Config{
-		OutPath: "../query",
+		OutPath: "../data/query",
 		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
 	})
-
-	gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	// todo 记得改dsn密码和用户
+	gormdb, _ := gorm.Open(mysql.Open("user:pwd@tcp(www.321go.top:50681)/my_treasure_doc?charset=utf8mb4&parseTime=True&loc=Local"))
 	g.UseDB(gormdb) // reuse your gorm db
 
 	// Generate basic type-safe DAO API for struct `model.User` following conventions
-	g.ApplyBasic(model.User{})
-
-	// Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
-	g.ApplyInterface(func(Querier) {}, model.User{}, model.Company{})
+	g.ApplyBasic(g.GenerateAllTable()...)
 
 	// Generate the code
 	g.Execute()
