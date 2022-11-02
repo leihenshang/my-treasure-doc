e8f72791dd7c13c6a15932e1b4d7ff9335cf6aaa
tangzq <t1339804847@163.com>
2022-11-02 17:08:34 +0800

    feat:完善sql debug模式

--- backend/service/mall/global/global.go
+++ backend/service/mall/global/global.go
@@ -71,11 +71,12 @@ func GlobalInit(srv srvType) {
 
 func initQuery() {
 	if CONFIG.App.IsRelease() {
+		ZAPSUGAR.Infof("初始化query，使用:release")
 		query.SetDefault(DB)
 	} else {
+		ZAPSUGAR.Infof("初始化query，使用:dev")
 		query.SetDefault(DB.Debug())
 	}
-
 }
 
 func initConf() {
@@ -107,9 +108,9 @@ func initMysql() {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold: time.Second, // Slow SQL threshold
-			LogLevel:      logger.Info, // Log level
-			Colorful:      true,        // Disable color
+			SlowThreshold: time.Second,   // Slow SQL threshold
+			LogLevel:      logger.Silent, // Log level
+			Colorful:      true,          // Disable color
 		},
 	)
 
