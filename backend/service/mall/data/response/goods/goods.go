08bc9a7a00d0e4ed25803b6238679f6ed114b7e9
tangzq <t1339804847@163.com>
2022-11-02 19:04:24 +0800

    feat:完善商品详情接口

--- backend/service/mall/data/response/goods/goods.go
+++ backend/service/mall/data/response/goods/goods.go
@@ -14,3 +14,28 @@ type GoodsEntity struct {
 	UpdatedAt string `json:"updatedAt"`
 	DeletedAt string `json:"deletedAt"`
 }
+
+type GoodsDetail struct {
+	GoodsEntity
+	// sku
+	Sku []*GoodsSkuEntity
+}
+
+type GoodsSkuEntity struct {
+	ID           int32       `json:"id"`
+	GoodsID      int32       `json:"goodsId"`
+	GoodsSpecIds string      `json:"goodsSpecIds"`
+	GoodsSpec    []*SpecDesc `json:"goodsSpec"`
+	Price        float64     `json:"price"`
+	Stock        int32       `json:"stock"`
+	CreatedAt    string      `json:"createdAt"`
+	UpdatedAt    string      `json:"updatedAt"`
+	DeletedAt    string      `json:"deletedAt"`
+}
+
+type SpecDesc struct {
+	SpecId  int32  `json:"specId"`
+	Spec    string `json:"spec"`
+	SpecVal string `json:"specVal"`
+	Units   string `json:"units"`
+}
--- backend/service/mall/internal/service/goods/goods.go
+++ backend/service/mall/internal/service/goods/goods.go
@@ -3,12 +3,13 @@ package goods
 import (
 	"context"
 	"errors"
-	"fastduck/treasure-doc/service/mall/data/model"
 	"fastduck/treasure-doc/service/mall/data/query"
 	goodsReq "fastduck/treasure-doc/service/mall/data/request/goods"
 	goodsResp "fastduck/treasure-doc/service/mall/data/response/goods"
 	"fastduck/treasure-doc/service/mall/global"
 	goodsDao "fastduck/treasure-doc/service/mall/internal/dao/goods"
+	"strconv"
+	"strings"
 )
 
 func GoodsList(ctx context.Context, f goodsReq.FilterGoodsList) (res *goodsResp.GoodsList, err error) {
@@ -42,13 +43,78 @@ func GoodsList(ctx context.Context, f goodsReq.FilterGoodsList) (res *goodsResp.
 	return
 }
 
-func GoodsDetail(ctx context.Context, f goodsReq.FilterGoodsDetail) (res *model.Good, err error) {
+func GoodsDetail(ctx context.Context, f goodsReq.FilterGoodsDetail) (res *goodsResp.GoodsDetail, err error) {
 	if f.GoodsId == 0 {
 		err = errors.New("商品id不能为空")
 		return
 	}
 
 	q := query.Good.WithContext(ctx)
-	res, err = q.Where(query.Good.ID.Eq(f.GoodsId)).First()
+	result, qErr := q.Where(query.Good.ID.Eq(f.GoodsId)).First()
+	if qErr != nil {
+		global.ZAPSUGAR.Errorf("GoodsDetail failed to get goods info.err:%+v", qErr)
+		err = errors.New("查询商品信息失败")
+		return
+	}
+
+	res = &goodsResp.GoodsDetail{
+		GoodsEntity: goodsResp.GoodsEntity{
+			ID:        result.ID,
+			Img:       result.Img,
+			GoodsName: result.GoodsName,
+			Quantity:  0,
+			CreatedAt: result.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: result.UpdatedAt.Format("2006-01-02 15:04:05"),
+			DeletedAt: result.DeletedAt.Time.Format("2006-01-02 15:04:05"),
+		},
+		Sku: []*goodsResp.GoodsSkuEntity{},
+	}
+
+	// sku 信息
+	sku, skuErr := query.GoodsSku.WithContext(ctx).Where(query.GoodsSku.GoodsID.Eq(res.ID)).Find()
+	if skuErr != nil {
+		global.ZAPSUGAR.Errorf("GoodsDetail failed to get goods sku.err:%+v", skuErr)
+	}
+
+	// 获取spec 描述
+	spec, specErr := query.GoodsSpec.WithContext(ctx).Where(query.GoodsSpec.GoodID.In(res.ID)).Find()
+	if specErr != nil {
+		global.ZAPSUGAR.Errorf("GoodsDetail failed to get goods spec.err:%+v", specErr)
+	}
+
+	specMap := make(map[int32][]string, 0)
+	for _, v := range spec {
+		specMap[v.ID] = []string{v.Spec, v.SpecVal, v.Units}
+	}
+
+	for _, v := range sku {
+		res.Sku = append(res.Sku, &goodsResp.GoodsSkuEntity{
+			ID:           v.ID,
+			GoodsID:      v.GoodsID,
+			GoodsSpecIds: v.GoodsSpecIds,
+			GoodsSpec: func() (s []*goodsResp.SpecDesc) {
+				goodsSpec := strings.Split(v.GoodsSpecIds, ",")
+				for _, v := range goodsSpec {
+					tempId, _ := strconv.Atoi(v)
+					if vv, ok := specMap[int32(tempId)]; ok {
+						s = append(s, &goodsResp.SpecDesc{
+							SpecId:  int32(tempId),
+							Spec:    vv[0],
+							SpecVal: vv[1],
+							Units:   vv[2],
+						})
+					}
+				}
+
+				return
+			}(),
+			Price:     v.Price,
+			Stock:     v.Stock,
+			CreatedAt: v.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: v.UpdatedAt.Format("2006-01-02 15:04:05"),
+			DeletedAt: v.DeletedAt.Time.Format("2006-01-02 15:04:05"),
+		})
+	}
+
 	return
 }
