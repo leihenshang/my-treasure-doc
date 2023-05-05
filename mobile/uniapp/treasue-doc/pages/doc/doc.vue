e1eb9e1c58fa0bace37b592d06679cb10c753689
tzq <t1339803847@163.com>
2023-05-05 14:51:54 +0800

    feat:add uniapp project file

---
+++ mobile/uniapp/treasue-doc/pages/doc/doc.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<h1>this is doc page</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
