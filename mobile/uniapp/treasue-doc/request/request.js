7d02975649cb3f108dd8ee5948decc7d7d137534
tangq <t1339803847@163.com>
2023-05-10 08:43:07 +0800

    feat:perfect request/request.js

--- mobile/uniapp/treasue-doc/request/request.js
+++ mobile/uniapp/treasue-doc/request/request.js
@@ -1,14 +1,33 @@
-const host = ""
+const host = "http://127.0.0.1:2021"
 
-function getDocList() {
-	uni.request({
-		url: host + "",
+async function getDocList() {
+	let finallRes = null;
+	 uni.request({
+		url: host + "/doc/list",
 		data: {
 
 		},
-		// header: {},
+		method: "POST",
+		header: {
+			"X-Token": "3b5b3d702a9637860ac351550859cd19"
+		},
 		success: (res) => {
-			console.log(res.data);
+			// console.log(res.data);
+			if (!res.data) {
+				finallRes = 'data is empty'
+			}
+			if (res.data.code > 0) {
+				finallRes = res.data.msg;
+			}
+
+			finallRes = res.data.data;
+			console.log(finallRes)
 		}
 	})
+
+	return finallRes
+}
+
+export {
+	getDocList
 }
