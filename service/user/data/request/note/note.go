1784698faf5869293a906e4226752facaa62f33d
tzq <t1339803847@163.com>
2024-11-18 15:59:34 +0800

    feat: update validate

--- service/user/data/request/note/note.go
+++ service/user/data/request/note/note.go
@@ -7,18 +7,18 @@ import (
 
 // CreateNoteRequest 创建文档
 type CreateNoteRequest struct {
-	Title    string         `json:"title" binding:"min=1,max=250"` // 标题
-	Content  string         `json:"content" binding:"required"`    // 文档内容
-	NoteType model.NoteType `json:"noteType" binding:"required"`   // 文档内容
-	IsTop    int            `json:"isTop" binding:""`              // 是否置顶
+	Title    string         `json:"title" binding:"max=250"`     // 标题
+	Content  string         `json:"content" binding:"required"`  // 文档内容
+	NoteType model.NoteType `json:"noteType" binding:"required"` // 文档内容
+	IsTop    int            `json:"isTop" binding:""`            // 是否置顶
 }
 
 // UpdateNoteRequest 更新文档
 type UpdateNoteRequest struct {
 	Id      int    `json:"id" binding:"required"`
-	Title   string `json:"title" binding:"min=1,max=250"` // 标题
-	Content string `json:"content" binding:""`            // 文档内容
-	IsTop   int    `json:"isTop" binding:""`              // 是否置顶
+	Title   string `json:"title" binding:"max=250"` // 标题
+	Content string `json:"content" binding:""`      // 文档内容
+	IsTop   int    `json:"isTop" binding:""`        // 是否置顶
 }
 
 type ListNoteRequest struct {
