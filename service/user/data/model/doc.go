e0564c672152b2881c8fb2f68761420085d0cb70
tzq <t1339803847@163.com>
2024-12-06 14:10:18 +0800

    feat: remove unnecessary fields

--- service/user/data/model/doc.go
+++ service/user/data/model/doc.go
@@ -6,13 +6,11 @@ type Doc struct {
 	Title     string    `gorm:"column:title;type:varchar(100);NOT NULL" json:"title"`                     // 标题
 	Content   string    `gorm:"column:content;type:text;NOT NULL" json:"content"`                         // 文档内容
 	DocStatus int       `gorm:"column:doc_status;type:tinyint(4);default:1;NOT NULL" json:"docStatus"`    // 1正常2审核中3禁用
-	ViewCount int       `gorm:"column:view_count;type:int(11);default:0;NOT NULL" json:"viewCount"`       // 查看次数
-	LikeCount int       `gorm:"column:like_count;type:int(11);default:0;NOT NULL" json:"likeCount"`       // 点赞次数
 	IsTop     int8      `gorm:"column:is_top;type:tinyint(4);default:0;NOT NULL" json:"isTop"`            // 是否置顶
 	Priority  int       `gorm:"column:priority;type:int(255);default:0;NOT NULL" json:"priority"`         // 优先级
 	GroupId   int64     `gorm:"column:group_id;type:bigint(20);default:0;NOT NULL" json:"groupId"`        // 分组id
-	ReadOnly  int8      `gorm:"column:read_only;type:tinyint(4);default:0;NOT NULL" json:"readOnly"`      // 分组id
-	Version   int       `gorm:"column:version;type:int(11);default:0;NOT NULL" json:"version"`            // 点赞次数
+	ReadOnly  int8      `gorm:"column:read_only;type:tinyint(4);default:0;NOT NULL" json:"readOnly"`      // 1-读写，2-只读
+	Version   int       `gorm:"column:version;type:int(11);default:0;NOT NULL" json:"version"`
 	GroupPath DocGroups `gorm:"-:all" json:"groupPath"`
 	IsPin     int       `gorm:"-" json:"isPin"`
 }
