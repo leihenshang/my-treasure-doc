df726d1a858d412a256dc96fd4c0c3376b938312
tzq <t1339803847@163.com>
2024-12-25 10:41:11 +0800

    feat(api/doc): update validate rules

--- service/user/data/request/doc/doc.go
+++ service/user/data/request/doc/doc.go
@@ -6,28 +6,28 @@ import (
 
 // CreateDocRequest 创建文档
 type CreateDocRequest struct {
-	Title   string `json:"title" binding:"max=250"` // 标题
-	Content string `json:"content" binding:""`      // 文档内容
-	GroupId string `json:"groupId" binding:""`
+	Title   string `json:"title" binding:"required,max=250,min=2"`
+	Content string `json:"content" binding:""`
+	GroupId string `json:"groupId" binding:"required,alphanum"`
 	IsTop   int8   `json:"isTop" binding:""`
 }
 
 // UpdateDocRequest 更新文档
 type UpdateDocRequest struct {
-	Id        string `json:"id" binding:"required"`
+	Id        string `json:"id" binding:"required,alphanum"`
 	GroupId   string `json:"groupId" binding:""`
-	Title     string `json:"title" binding:"max=250"` // 标题
-	Content   string `json:"content" binding:""`      // 文档内容
-	IsTop     int8   `json:"isTop" binding:""`        // 是否置顶
-	IsRecover bool   `json:"isRecover" binding:""`
+	Title     string `json:"title" binding:"required,max=250,min=2"`
+	Content   string `json:"content" binding:""`
+	IsTop     int8   `json:"isTop" binding:""`
+	IsRecover bool   `json:"isRecover" binding:"boolean"`
 	IsPin     int8   `json:"isPin" binding:""`
 	ReadOnly  int8   `json:"readOnly" binding:""`
-	Version   *int   `json:"version" binding:"required"`
+	Version   *int   `json:"version" binding:"required,alphanum"`
 }
 
 // DeleteDocRequest 更新文档
 type DeleteDocRequest struct {
-	Id string `json:"id" binding:"required"`
+	Id string `json:"id" binding:"required,alphanum"`
 }
 
 type ListDocRequest struct {
