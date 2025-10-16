f0c7a061a5d47addd44c1d2e09f0c687b119d309
tzq <t1339803847@163.com>
2025-10-16 14:32:55 +0800

    Update service.go

--- module/reverse_index/service/service.go
+++ module/reverse_index/service/service.go
@@ -1,8 +1,15 @@
 package service
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
 	"fastduck/treasure-doc/module/reverse_index/comm/gid"
 	"fastduck/treasure-doc/module/reverse_index/index"
+	"fmt"
+	"hash"
 )
 
 func Index(content ...string) error {
@@ -31,4 +38,27 @@ func Search(keyword string) ([]string, error) {
 
 func List() any {
 	return index.GetIndexCache().List()
+}
+
+func ComputeHash(algorithm string, data []byte) (string, error) {
+	var h hash.Hash
+	switch algorithm {
+	case "md5":
+		h = md5.New()
+	case "sha1":
+		h = sha1.New()
+	case "sha256":
+		h = sha256.New()
+	case "sha512":
+		h = sha512.New()
+	default:
+		return "", fmt.Errorf("unsupported hash algorithm: %s", algorithm)
+	}
+
+	_, err := h.Write(data)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
